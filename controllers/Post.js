7d8cb9cf46b599318f0ae72f0730c6d4bcb006f9
Ahmed Morsy <morsyahmed402@gamil.com>
2023-11-05 18:32:27 +0200

    image schema

--- controllers/Post.js
+++ controllers/Post.js
@@ -26,6 +26,7 @@ const readPost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (!post) return res.status(400).json("No Post Found");
+
     res.status(200).json(post);
   } catch (error) {
     res.status(500).json(error);
